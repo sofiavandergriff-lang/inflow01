0be4f05f32b709417b918e4d0502e2fbbe0263f6
sofiavandergriff-lang <sofiavandergriff@gmail.com>
2025-10-01 21:34:47 +0100

    Updated LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -222,7 +222,7 @@ export default function LoginPage() {
         
         <div className="flex flex-col justify-center items-center text-center px-12 relative z-10">
           {/* Header Text */}
-          <div className="mb-16">
+          <div className="mb-26">
             <h2 className="text-4xl font-black text-white mb-4">
               Effortlessly manage your team and operations.
             </h2>
